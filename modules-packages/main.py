0cdea7d9a943233ca95d5bc316394cca18357a8e
Dmitry_Kitty <dnikitin@student.agh.edu.pl>
2025-04-06 21:16:53 +0200

    import theory

---
+++ modules-packages/main.py
@@ -0,0 +1,12 @@
+import pprint  # import
+from math import sqrt  # import jednej konkretnej funkcji - nie trzeba pisac math przed
+from math import pi as p, ceil  # import funkcji z aliasem
+import time as t #import modułu z aliasem
+from tabnanny import * #import wszystkich imion co są do naszego modułu, lepiej nie robic
+
+
+pprint.pprint(globals())
+b = sqrt(9)
+print(p)
+a = ceil(sqrt(9))
+t = t.time()
